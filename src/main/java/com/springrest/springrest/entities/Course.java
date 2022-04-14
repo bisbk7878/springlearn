6331e3e070d660fdb870ebd839deaf4774a4e1ac
Bishal <bishalbk7878@gmail.com>
2022-04-13 19:28:07 -0800

    VariableUpdatess

--- src/main/java/com/springrest/springrest/entities/Course.java
+++ src/main/java/com/springrest/springrest/entities/Course.java
@@ -9,7 +9,7 @@ import javax.persistence.Table;
 public class Course {
   @Override
 	public String toString() {
-		return "Course [id=" + id + ", title=" + title + ", description=" + description + "]";
+		return "Course [id=" + id + ", title=" + title + ", descrip=" + descrip + "]";
 	}
 public long getId() {
 		return id;
@@ -23,24 +23,24 @@ public long getId() {
 	public void setTitle(String title) {
 		this.title = title;
 	}
-	public String getDescription() {
-		return description;
+	public String getdescrip() {
+		return descrip;
 	}
-	public void setDescription(String description) {
-		this.description = description;
+	public void setdescrip(String descrip) {
+		this.descrip = descrip;
 	}
 public Course() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-public Course(long id, String title, String description) {
+public Course(long id, String title, String descrip) {
 		super();
 		this.id = id;
 		this.title = title;
-		this.description = description;
+		this.descrip = descrip;
 	}
    @Id
   private long id;
   private String title;
-  private String description;
+  private String descrip;
 }
